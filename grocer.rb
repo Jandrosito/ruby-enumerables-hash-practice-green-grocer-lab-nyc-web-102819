369976232eb576fab1e1519e430cbf19c57baa47
Edward Castillo <castilloedwarda@gmail.com>
2019-10-25 14:57:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,7 +17,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]
     if cart.has_key?[item]
-      if cart[item][:count] >= coupon[:num] && !cart.has_key?["#{item} W/COUPON"] = {price: coupon[:cost] / coupon
+      if cart[item][:count] >= coupon[:num] && !cart.has_key?["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num]
 end
 
 def apply_clearance(cart)
