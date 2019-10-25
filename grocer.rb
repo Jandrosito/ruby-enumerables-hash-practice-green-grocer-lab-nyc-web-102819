3bff90485bee07649f457a4769f032bd829f6504
Edward Castillo <castilloedwarda@gmail.com>
2019-10-25 15:31:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,11 +14,9 @@ final_hash
 end
 
 def apply_coupons(cart, coupons) 
-  
   coupons.each do |coupon| 
     coupon.each do |attribute, value| 
       name = coupon[:item] 
-    
       if cart[name] && cart[name][:count] >= coupon[:num] 
         if cart["#{name} W/COUPON"] 
           cart["#{name} W/COUPON"][:count] += 1 
@@ -26,7 +24,6 @@ def apply_coupons(cart, coupons)
           cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
           :clearance => cart[name][:clearance], :count => 1} 
         end 
-  
       cart[name][:count] -= coupon[:num] 
     end 
   end 
