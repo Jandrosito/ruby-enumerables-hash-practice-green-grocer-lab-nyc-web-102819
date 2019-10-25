32944b5376bd9d0eb908b643e34484766be189e0
Edward Castillo <castilloedwarda@gmail.com>
2019-10-25 14:36:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,7 @@
 def consolidate_cart(cart)
  final_hash = {}
  cart.each do |element_hash|
-   element_name = element_hash
+   element_name = element_hash.keys[0]
 end
 
 def apply_coupons(cart, coupons)
