d330e22de9c288ab3a6cc5a010197a8e12d75755
Edward Castillo <castilloedwarda@gmail.com>
2019-10-25 15:09:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,6 +23,8 @@ def apply_coupons(cart, coupons)
         cart["#{item} W/COUPON"][:count] += coupon[:num]
       end
       cart[item][:count] -= coupon[:num]
+    end
+  end
 end
 
 def apply_clearance(cart)
